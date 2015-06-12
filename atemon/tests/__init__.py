d594c2c5ac071d6e404aacfd0ec7acc1984f8d2a
Agile Technology Engineers Monastery <pope@atemon.com>
2015-06-12 11:56:41 +0530

    Initiated test

---
+++ atemon/tests/__init__.py
@@ -0,0 +1,23 @@
+def setup_module(cls):
+    pass
+
+
+def teardown_module(cls):
+    pass
+
+
+class TestBase(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
